cd2eeee29800c776845b22069128732d64710bf6
Qa-krushna <rashwini510@gmail.com>
2022-07-30 19:41:59 +0530

    Added: Display day of week using case statement

---
+++ DayOfWeekCaseStatement.js
@@ -0,0 +1,25 @@
+let day = Math.round(Math.random() * 7);
+switch (day){
+    case 0:
+       console.log("The day is: SUNDAY");
+       break;
+    case 1:
+        console.log("The day is: MONDAY");
+        break;
+    case 2:
+        console.log("The day is: TUESDAY");
+        break;
+    case 3:
+        console.log("The day is: WEDNESDAY");
+        break;
+    case 4:
+        console.log("The day is: THURSDAY");
+        break;
+    case 5:
+        console.log("The day is: FRIDAY");
+        break;
+    case 6:
+        console.log("The day is: SATURDAY");
+        break;                     
+}
+
