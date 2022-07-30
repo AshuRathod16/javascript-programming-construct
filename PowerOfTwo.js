4d64dba3d30cc21a1c07e27888c5b6b990b2b13f
Qa-krushna <rashwini510@gmail.com>
2022-07-30 20:27:56 +0530

    Added: Ability to display power of two

---
+++ PowerOfTwo.js
@@ -0,0 +1,10 @@
+let num = 10;
+for(let i = 1; i <= num; i++){
+    let powerOfTable = Math.pow(2,i);
+    console.log("Power of two" + " " + "is" + " " + powerOfTable);
+}
+// let num=8;
+// for(let i = 1 ; i<= num;i++){
+//       let powerOfTable= Math.pow(2,i);
+//       console.log("2 PowerOf "+i +" is "+powerOfTable);
+// }
